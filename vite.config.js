e21d809f61e433822743a1021b390905b7a26639
ppxt-code <xuantrang.phamphu@gmail.com>
2025-12-01 17:50:18 +0100

    git hub pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ import react from '@vitejs/plugin-react'
 })*/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'development' ? '/' : '/nom-du-repo/'
+  base: mode === 'production' ? '/cvppxt/' : '/'
 }))
 
