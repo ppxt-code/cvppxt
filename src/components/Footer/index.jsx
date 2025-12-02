66b1921e9abd86f01d95ee90f3fafb5cc3fe1075
ppxt-code <xuantrang.phamphu@gmail.com>
2025-12-02 20:40:40 +0100

    lien sur LinkedIn

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,10 @@
+
+import linkedinIcon  from '../../assets/linkedin.webp'; 
 function Footer () {
-    return (<div className="footerWrapper"></div>);
+    return (<div className="footerWrapper">
+                <a href="https://www.linkedin.com/in/xuantrang-pham-phu-8b4431398/" target="_blank" rel="noopener noreferrer">
+                    <img className="contactIcon" src={linkedinIcon} alt="icone linkedin"/>
+                </a>
+            </div>);
 }
 export default Footer;
