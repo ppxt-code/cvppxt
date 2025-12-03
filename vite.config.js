65e8547e731120a632974bd712a5860c3f0bf153
ppxt-code <xuantrang.phamphu@gmail.com>
2025-12-03 17:59:09 +0100

    404 GitHub Pages vite.config.js  et 404.html copie de index.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy' 
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      
+      // GitHub Pages specific config
+      targets: [
+        {
+          src: '404.html',  // Fichier 404.html à la racine du projet
+          dest: '',         // Copier à la racine de dist/
+        },
+      ],
+    }),
+  ],
   base: mode === 'production' ? '/cvppxt/' : '/',
 
   // GitHub Pages specific config
