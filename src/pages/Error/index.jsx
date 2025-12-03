102ad1fb17ea494609ffe5e6e503c6c2326b9091
ppxt-code <xuantrang.phamphu@gmail.com>
2025-12-03 17:18:02 +0100

    404 GitHub Pages vite.config.js

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -2,13 +2,13 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 function Error() {
-    
+  const basename = import.meta.env.PROD ? '/cvppxt/' : '/'; 
   return (
     <div className="errorWrapper">
       <Header />
       <main className="errorBody">
         <h1>Erreur 404</h1><br/>
-        <img src="/images/404error.gif" alt="animation 404" />
+        <img src={`${basename}images/404error.gif`} alt="animation 404" />
       </main>
       <Footer />
     </div>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-/*export default defineConfig({
-  plugins: [react()],
-})*/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/cvppxt/' : '/'
+  base: mode === 'production' ? '/cvppxt/' : '/',
+
+  // GitHub Pages specific config
+  build: {
+    rollupOptions: {
+      output: {
+        // chunkFileNames, entryFileNames, assetFileNames
+      }
+    }
+  }
 }))
 
