63a4ae946372b3cb39d0d11b24f2914cd5bf7217
ppxt-code <xuantrang.phamphu@gmail.com>
2025-12-06 17:00:36 +0100

    pour GitHub Pages BrowserRouter -> HashRouter (main)

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-// import './scss/style.scss'
+import { HashRouter } from 'react-router-dom' 
 import './css/styles.css'
 import App from './App.jsx'
 const basename = import.meta.env.PROD ? '/cvppxt/' : '/';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter basename={basename}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
