eb4c9d64c16fd6295335786a9b75d693e245b209
ppxt-code <xuantrang.phamphu@gmail.com>
2025-12-09 10:27:57 +0100

    on remet BrowserRouter à la place de hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom' 
+//import { HashRouter } from 'react-router-dom' 
 import './css/styles.css'
 import App from './App.jsx'
-/*
+
 import { BrowserRouter } from 'react-router-dom' 
 const basename = import.meta.env.PROD ? '/cvppxt/' : '/';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename={basename}>
+    <BrowserRouter basename={basename}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
-)*/
-createRoot(document.getElementById('root')).render(
+)
+/*createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
-)
+)*/
